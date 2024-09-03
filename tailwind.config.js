dd9c0da0bc149d54786623c7c6f5462c01487b73
helloleesul <suerish.e@gmail.com>
2024-09-03 14:28:54 +0900

    :wrench: tailwindcss colors 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ export default {
     fontFamily: {
       body: ["SUIT"],
     },
+    colors: {
+      primary: "#1d2934",
+      secondary: "#00adff",
+      tertiary: "#00d392",
+      error: "#ff6252",
+      gray0: "#f8fafc",
+      gray100: "#f5f6f8",
+      gray200: "#e8eef2",
+      gray300: "#d0d5d9",
+      white: "#ffffff",
+      black: "#000000",
+    },
   },
   plugins: [],
 };
