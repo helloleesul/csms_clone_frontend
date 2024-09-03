4aa512430c9cc71338cc820024b1e5724758b790
helloleesul <suerish.e@gmail.com>
2024-09-03 16:17:52 +0900

    :sparkles: Button 스토리 추가

---
+++ src/shared/ui/Button.tsx
@@ -0,0 +1,31 @@
+import { ComponentProps, ReactNode } from "react";
+
+import { twMerge } from "tailwind-merge";
+
+type ButtonFontProps = ComponentProps<"button"> & {
+  color: "white" | "success" | "primary" | "secondary" | "danger" | "gray";
+  children: ReactNode;
+};
+
+const colorVariants = {
+  white: "bg-white text-black",
+  success: "bg-success",
+  primary: "bg-primary",
+  secondary: "bg-secondary",
+  danger: "bg-danger",
+  gray: "bg-gray200 text-black",
+};
+
+const Button = ({ color, children }: ButtonFontProps) => {
+  return (
+    <button
+      className={twMerge(
+        `font-gotham font-medium text-2xl outline-none px-6 py-5 rounded-lg text-white ${colorVariants[color]}`,
+      )}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
