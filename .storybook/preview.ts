fd1160d6d8b71dd25a88686ec180d1d041da59c1
helloleesul <suerish.e@gmail.com>
2024-09-03 16:15:39 +0900

    :bento: gotham font assets 추가

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,7 @@ import type { Preview } from "@storybook/react";
 import "@sun-typeface/SUIT/fonts/static/woff2/SUIT.css";
 
 import "../src/index.css";
+import "../src/shared/assets/fonts/gotham/Gotham.css";
 
 const preview: Preview = {
   parameters: {
