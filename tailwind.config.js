05843226a21bced069e22124a01e39be6cd4f86f
helloleesul <suerish.e@gmail.com>
2024-09-03 16:16:03 +0900

    :wrench: tailwindcss colors 수정, font 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    fontFamily: {
-      body: ["SUIT"],
-    },
-    colors: {
-      primary: "#1d2934",
-      secondary: "#00adff",
-      tertiary: "#00d392",
-      error: "#ff6252",
-      gray0: "#f8fafc",
-      gray100: "#f5f6f8",
-      gray200: "#e8eef2",
-      gray300: "#d0d5d9",
-      white: "#ffffff",
-      black: "#000000",
+    extend: {
+      fontFamily: {
+        suit: ["SUIT"],
+        gotham: ["Gotham"],
+      },
+      colors: {
+        primary: "#1d2934",
+        secondary: "#00adff",
+        success: "#00d392",
+        danger: "#ff6252",
+        gray0: "#f8fafc",
+        gray100: "#f5f6f8",
+        gray200: "#e8eef2",
+        gray300: "#d0d5d9",
+        white: "#ffffff",
+        black: "#000000",
+      },
     },
   },
   plugins: [],
