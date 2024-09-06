83e4bec59b0a75e016376ce3563c59d4499ff2e5
helloleesul <suerish.e@gmail.com>
2024-09-06 13:31:13 +0900

    :label: auth atore type 추가

--- src/shared/model/useAuthStore.ts
+++ src/shared/model/useAuthStore.ts
@@ -1,18 +1,38 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
+import { Tokens } from "./types.ts";
+
 interface AuthState {
   isAuthenticated: boolean;
-  login: () => void;
-  logout: () => void;
+  tokens: Tokens;
+  setAuth: (tokens: Tokens) => void;
+  clearAuth: () => void;
+  updateAccessToken: (accessToken: Pick<Tokens, "accessToken">) => void;
 }
 
 const useAuthStore = create<AuthState>()(
   persist(
     (set) => ({
       isAuthenticated: false,
-      login: () => set({ isAuthenticated: true }),
-      logout: () => set({ isAuthenticated: false }),
+      tokens: {
+        accessToken: "",
+        refreshToken: "",
+      },
+      setAuth: (tokens) => set({ isAuthenticated: true, tokens }),
+      clearAuth: () =>
+        set({
+          isAuthenticated: false,
+          tokens: {
+            accessToken: "",
+            refreshToken: "",
+          },
+        }),
+      updateAccessToken: ({ accessToken }) =>
+        set((state) => ({
+          ...state,
+          tokens: { ...state.tokens, accessToken },
+        })),
     }),
     {
       name: "auth-storage", // name of the item in the storage (must be unique)
