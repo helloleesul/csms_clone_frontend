9185fe43c7021e2a7122e560f532d29dac4bbfb3
helloleesul <suerish.e@gmail.com>
2024-09-09 10:03:35 +0900

    :wrench: 라우터 경로 수정

--- src/shared/consts/paths.ts
+++ src/shared/consts/paths.ts
@@ -1,8 +1,23 @@
 export const PATHS = {
   ROOT: "/",
   AUTH: { LOGIN: "/login" },
+  DASHBOARD: { BASE: "/dashboard" },
   CHARGING_INFRA: {
     BASE: "/charging-infra",
     EV_STATION: "/charging-infra/ev-station",
+    EV_CHARGER: "/charging-infra/ev-charger",
+  },
+  CHARGING_HISTORY: { BASE: "/charging-history" },
+  USER: { BASE: "/user", ONLINE_USER: "/user/online-user" },
+  CHARGING_PRICE: {
+    BASE: "/charging-price",
+    PRICE_LIST: "/charging-price/list",
+    PRICE_HISTORY: "/charging-price/history",
+  },
+  CUSTOMER_SERVICE: { BASE: "/customer-service" },
+  SETTINGS: {
+    BASE: "/settings",
+    NOTICE: "/settings/notice",
+    ADMIN_USER: "/settings/admin-user",
   },
 };
